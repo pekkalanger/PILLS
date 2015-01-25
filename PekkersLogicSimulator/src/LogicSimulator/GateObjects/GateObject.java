423bfa22de8e7b70f9d60fbf80aa8ba92540dc22
axel Nygard <axel.nygard@gmail.com>
2015-01-25 13:31:37 +0200

    removed some comments from GateObject.java

--- PekkersLogicSimulator/src/LogicSimulator/GateObjects/GateObject.java
+++ PekkersLogicSimulator/src/LogicSimulator/GateObjects/GateObject.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package LogicSimulator.GateObjects;
 
 import Logic.Gate;
@@ -15,10 +10,6 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.shape.Line;
 import javafx.scene.shape.Rectangle;
 
-/**
- *
- * @author PEKKA
- */
 public class GateObject {
         
     double x;
