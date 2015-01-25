3f286b47031ea2bac21dd72c94ab1c8e1f17c1f2
axel Nygard <axel.nygard@gmail.com>
2015-01-25 12:39:38 +0200

    added init() to Textures for preloading and other future stuff

--- PekkersLogicSimulator/src/LogicSimulator/Main.java
+++ PekkersLogicSimulator/src/LogicSimulator/Main.java
@@ -228,6 +228,7 @@ public class Main extends Application {
     }
       
     @Override public void start(Stage primaryStage) throws Exception {
+        Textures.init();
         Globals.main = this;
         init(primaryStage);
         primaryStage.show();
