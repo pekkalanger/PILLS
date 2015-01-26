96a6ab05d1ae8c3e92906d5fe201a6204c9a81a1
axel Nygard <axel.nygard@gmail.com>
2015-01-26 14:02:55 +0200

    updated license or whatever

--- PekkersLogicSimulator/src/LogicSimulator/GateObjects/TheProbulatorObject.java
+++ PekkersLogicSimulator/src/LogicSimulator/GateObjects/TheProbulatorObject.java
@@ -14,13 +14,12 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package LogicSimulator;
+package LogicSimulator.GateObjects;
 
 /**
  *
  * @author PEKKA
  */
-public class Globals {
-    public static Main main;
+public class TheProbulatorObject {
     
 }
